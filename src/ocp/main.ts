cd383eeed819004d8e5bcb589c2b93d869fa1c51
Daniel Castro <danielmccastro@gmail.com>
2023-09-01 17:42:47 -0300

    OCP

---
+++ src/ocp/main.ts
@@ -0,0 +1,31 @@
+/* open/closed principle
+entidades devem estar abertas para extensao, mas fechadas para modificacao.
+*/
+import { Messaging } from './services/messaging';
+import { Order } from './entities/order';
+import { Persistency } from './services/persistency';
+import { Product } from './entities/product';
+import { ShoppingCart } from './entities/shopping-cart';
+import {
+  NoDiscount,
+  /* FiftyPercentDiscount, TenPercentDiscount,*/
+} from './entities/discount';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lapis', 1.9));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
