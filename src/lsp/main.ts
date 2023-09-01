2440d139b6118666f278c769f2a1c59a631ee753
Daniel Castro <danielmccastro@gmail.com>
2023-09-01 17:57:21 -0300

    LSP

---
+++ src/lsp/main.ts
@@ -0,0 +1,31 @@
+/* liskov substitution principle ("subtipos precisam ser substituives por seus tipos de base")
+ex: se meu programa espera um Animal, algo do tipo Dog (que herda de Animal) deve servir como qualquer outro Animal
+ */
+import { Messaging } from './services/messaging';
+import { Order } from './entities/order';
+import { Persistency } from './services/persistency';
+import { Product } from './entities/product';
+import { ShoppingCart } from './entities/shopping-cart';
+import {
+  NoDiscount,
+  /* FiftyPercentDiscount, TenPercentDiscount,*/
+} from './entities/discount';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lapis', 1.9));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
